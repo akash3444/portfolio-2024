2d53287ca6e4f70a8f6e3a3ba2dfa92d054b967e
Akash Moradiya <akashmoradiya3444@gmail.com>
2024-08-15 11:59:24 +0530

    refactor: update hero section UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import About from "@/components/About";
-import { HeroAbstract } from "@/components/Abstracts";
 import ContactMe from "@/components/ContactMe";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
@@ -13,24 +12,23 @@ export default function Home() {
   return (
     <>
       <div className="relative overflow-hidden">
-        <div
-          style={{ backgroundImage: "url(/grid-line.svg)" }}
-          className="absolute inset-0"
+        <img
+          src="/background-texture.jpg"
+          className="absolute inset-0 h-full w-full object-cover opacity-15"
         />
         <Section
           background="dark"
-          className="relative z-[1] pt-0 md:pt-0 lg:min-h-[100dvh]"
+          className="relative z-[1] min-h-[100dvh] pt-0 md:pt-0"
         >
           <Navbar />
           {/* Hero section */}
-          <div className="items-center gap-20 md:flex lg:mt-24 lg:justify-between">
-            <HeroAbstract className="h-80 w-full lg:h-auto lg:max-w-md" />
-            <div className="mt-8 md:max-w-xl">
+          <div className="mt-20 items-center gap-20 md:mt-24 md:flex lg:justify-between">
+            <div className="mx-auto mt-8 text-center">
               <Subtitle>Akash Moradiya</Subtitle>
-              <h1 className="mt-4 text-5xl font-bold leading-tight text-foreground lg:text-6xl lg:leading-tight">
+              <h1 className="mt-4 text-4xl font-bold leading-tight text-foreground sm:text-5xl md:mx-auto md:max-w-2xl md:text-6xl lg:text-7xl lg:leading-tight">
                 Crafting Engaging Web Experiences
               </h1>
-              <p className="mt-4 text-lg leading-7 text-muted-foreground">
+              <p className="mt-4 max-w-3xl leading-7 text-muted-foreground sm:text-lg">
                 I&apos;m Akash Moradiya, a front-end engineer with 2.5+ years of
                 experience in React.js and Next.js. I specialize in creating
                 efficient, scalable, and user-friendly applications.
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,10 +18,10 @@ const About = () => {
         <VersionControl className="w-full shrink-0 md:w-1/2 md:md:max-w-lg" />
         <div>
           <Subtitle>About Me</Subtitle>
-          <h3 className="text-foreground mt-6 text-4xl">
+          <h3 className="mt-6 text-4xl text-foreground">
             Professional Web Developer
           </h3>
-          <p className="text-muted-foreground mt-4 text-[17px] text-base leading-relaxed">
+          <p className="mt-4 text-[17px] text-base leading-relaxed text-muted-foreground">
             I excel at transforming wireframes into polished, responsive web
             interfaces, leveraging modern UI libraries such as Tailwind CSS and
             Shadcn UI. My expertise also includes integrating APIs to ensure
@@ -31,10 +31,10 @@ const About = () => {
           </p>
 
           <div className="mt-6">
-            <h6 className="text-foreground mb-2 text-lg font-semibold">
+            <h6 className="mb-2 text-lg font-semibold text-foreground">
               My Tech Stack
             </h6>
-            <div className="text-primary flex items-center gap-4">
+            <div className="flex items-center gap-4 text-primary">
               <ReactIcon />
               <NextIcon />
               <TypeScriptIcon />
@@ -47,22 +47,22 @@ const About = () => {
         </div>
       </div>
 
-      <div className="divide-border border-border mt-14 grid grid-cols-1 divide-y rounded-lg border md:grid-cols-3 md:divide-x md:divide-y-0">
+      <div className="mt-14 grid grid-cols-1 divide-y divide-border rounded-lg border border-border md:grid-cols-3 md:divide-x md:divide-y-0">
         <div className="px-7 py-12">
-          <h4 className="text-foreground text-4xl">15+</h4>
-          <h5 className="text-muted-foreground mt-3 text-lg font-medium">
+          <h4 className="text-4xl text-foreground">15+</h4>
+          <h5 className="mt-3 text-lg font-medium text-muted-foreground">
             Projects completed
           </h5>
         </div>
         <div className="px-7 py-12">
-          <h4 className="text-foreground text-4xl">2.5+</h4>
-          <h5 className="text-muted-foreground mt-3 text-lg font-medium">
+          <h4 className="text-4xl text-foreground">2.5+</h4>
+          <h5 className="mt-3 text-lg font-medium text-muted-foreground">
             Years of experience
           </h5>
         </div>
         <div className="px-7 py-12">
-          <h4 className="text-foreground text-4xl">100%</h4>
-          <h5 className="text-muted-foreground mt-3 text-lg font-medium">
+          <h4 className="text-4xl text-foreground">100%</h4>
+          <h5 className="mt-3 text-lg font-medium text-muted-foreground">
             Client satisfaction
           </h5>
         </div>
